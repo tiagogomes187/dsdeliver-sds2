adc4b884878ce92ed484d7c73b93c8eec9785d92
Tiago Gomes <tiagogomes187@gmail.com>
2021-01-10 23:39:10 -0300

    Removi o imports não usados

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,7 +1,6 @@
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
-import Navbar from "./Navbar";
 import Routes from "./Routes";
 
 function App() {
