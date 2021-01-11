654e00df030d6ab43f47525e57510ce710fbe532
Tiago Gomes <tiagogomes187@gmail.com>
2021-01-11 15:17:32 -0300

    Fix total

--- backend/src/main/java/com/devsuperior/dsdeliver/dto/OrderDTO.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/dto/OrderDTO.java
@@ -12,27 +12,31 @@ import com.devsuperior.dsdeliver.entities.OrderStatus;
 public class OrderDTO implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	private Long id;
 	private String address;
 	private Double latitude;
 	private Double longitude;
 	private Instant moment;
 	private OrderStatus status;
-	
-	private List<ProductDTO> products = new ArrayList<>();
-	
-	public OrderDTO() {}
+	private Double total;
 
-	public OrderDTO(Long id, String address, Double latitude, Double longitude, Instant moment, OrderStatus status) {
+	private List<ProductDTO> products = new ArrayList<>();
+
+	public OrderDTO() {
+	}
+
+	public OrderDTO(Long id, String address, Double latitude, Double longitude, Instant moment, OrderStatus status,
+			Double total) {
 		this.id = id;
 		this.address = address;
 		this.latitude = latitude;
 		this.longitude = longitude;
 		this.moment = moment;
 		this.status = status;
+		this.total = total;
 	};
-	
+
 	public OrderDTO(Order entity) {
 		id = entity.getId();
 		address = entity.getAddress();
@@ -40,8 +44,8 @@ public class OrderDTO implements Serializable {
 		longitude = entity.getLongitude();
 		moment = entity.getMoment();
 		status = entity.getStatus();
-		products = entity.getProducts().stream()
-				.map(x -> new ProductDTO(x)).collect(Collectors.toList());
+		total = entity.getTotal();
+		products = entity.getProducts().stream().map(x -> new ProductDTO(x)).collect(Collectors.toList());
 	}
 
 	public Long getId() {
@@ -92,11 +96,16 @@ public class OrderDTO implements Serializable {
 		this.status = status;
 	}
 
+	public Double getTotal() {
+		return total;
+	}
+
+	public void setTotal(Double total) {
+		this.total = total;
+	}
+
 	public List<ProductDTO> getProducts() {
 		return products;
 	};
-	
-	
-	
 
 }
