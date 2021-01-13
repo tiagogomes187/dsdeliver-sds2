f6e6b19e18f1594a68c85d66b17211697b13cbbb
Tiago Gomes <tiagogomes187@gmail.com>
2021-01-12 21:54:31 -0300

    Mobile application successfully completed.

--- front-mobile/src/Orders/index.tsx
+++ front-mobile/src/Orders/index.tsx
@@ -5,20 +5,37 @@ import Header from "../Header";
 import { Order } from "../types";
 import OrderCard from './../OrderCard/index';
 import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
+import { useIsFocused, useNavigation } from "@react-navigation/native";
 
 function Orders() {
 
   const [orders, setOrders] = useState<Order[]>([]);
   const [isLoading, setIsLoading] = useState(false);
+  const navigation = useNavigation();
+  const isFocused = useIsFocused();
 
-  useEffect(() => {
+  const fetchData = () => {
     setIsLoading(true);
     fetchOrders()
     .then(response => setOrders(response.data))
     .catch(() => Alert.alert('Houve um erro ao buscar os pedidos!'))
     .finally(() => setIsLoading(false));
+  }
 
-  }, []);
+  useEffect(() => {
+    if (isFocused) {
+      fetchData();
+    }
+  }, [isFocused]);
+
+    const handleOnPress = (order: Order) => {
+      navigation.navigate('OrdersDetails', {
+        order
+      });
+    }
+
+
+
   return (
     <>
       <Header />
@@ -27,7 +44,8 @@ function Orders() {
           <Text style={styles.text}>Buscando pedidos...</Text>
         ) : (
           orders.map(order =>(
-            <TouchableWithoutFeedback key={order.id}>
+            <TouchableWithoutFeedback key={order.id} 
+            onPress={() => handleOnPress(order)}>
               <OrderCard order={order} />          
             </TouchableWithoutFeedback>
           ))
--- front-mobile/src/Routes.tsx
+++ front-mobile/src/Routes.tsx
@@ -3,6 +3,8 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import Home from "./Home";
 import Orders from "./Orders";
+import OrdersDetails from "./OrderDetails";
+
 
 const Stack = createStackNavigator();
 
@@ -14,6 +16,7 @@ function Routes(){
             <Stack.Navigator headerMode="none" screenOptions={{cardStyle: {backgroundColor: '#FFF'}}} >
                 <Stack.Screen name="Home" component={Home}></Stack.Screen>
                 <Stack.Screen name="Orders" component={Orders}></Stack.Screen>
+                <Stack.Screen name="OrdersDetails" component={OrdersDetails}></Stack.Screen>
             </Stack.Navigator>
         </NavigationContainer>
     )
